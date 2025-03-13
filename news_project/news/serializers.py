87bfa8f445befb62607497211fa729e523fbcc3b
Leanstix <leanstixx@gmail.com>
2025-03-14 00:15:00 +0100

    working the templates

--- news_project/news/serializers.py
+++ news_project/news/serializers.py
@@ -7,17 +7,19 @@ class TagSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class NewsSerializer(serializers.ModelSerializer):
-    tags = TagSerializer(many=True, read_only=True)
+    tags = serializers.PrimaryKeyRelatedField(many=True, queryset=Tag.objects.all())
     likes_count = serializers.SerializerMethodField()
 
     class Meta:
         model = News
-        fields = '__all__'
+        fields = ['id', 'title', 'text', 'image', 'tags', 'created_at', 'views', 'likes_count']
 
     def get_likes_count(self, obj):
         return obj.like_set.filter(is_like=True).count()
 
 class LikeSerializer(serializers.ModelSerializer):
+    user = serializers.ReadOnlyField(source='user.username')  # Show username instead of ID
+
     class Meta:
         model = Like
-        fields = '__all__'
+        fields = ['id', 'user', 'news', 'is_like']
--- news_project/news_project/settings.py
+++ news_project/news_project/settings.py
@@ -19,6 +19,8 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
